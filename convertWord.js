a9a979d91b2344e63fad2fe83f84136b4f7d48c0
Endy <endy@Endys-MacBook-Pro.local>
2020-12-22 08:40:03 +0700

    init

---
+++ convertWord.js
@@ -0,0 +1,12 @@
+// Ubah function convertWords untuk mengkonversi huruf kecil menjadi besar
+// & sebalik nya
+
+function convertWords(kalimat) {
+  // you can only write your code here!
+}
+
+// TEST CASES
+console.log(convertWords('Hello World')); // "hELLO wORLD"
+console.log(convertWords('jajan pisang goreng di pasar'));
+console.log(convertWords('saya PaHaM Jav4Scr1pttt'));
+console.log(convertWords('HUJ4n, 3n4k M4kaN b4$0'));
---
+++ loopObject.js
@@ -0,0 +1,29 @@
+// ubah function convertMe dengan parameter berupa multidimensi Array
+// yg berisi firstName, lastName, gender & birth year.
+
+// convertMe akan me return sebuah object yg mempunyai property:
+// firstName,
+// lastName,
+// gender,
+// age
+
+// value age di dapatkan, dengan mengurangi tahun saat ini dengan tahun lahir.
+// Jika birth year tidak di isi, maka tampilkan 'invalid date'
+
+function convertMe(arr) {
+  // you can only write your code here!
+}
+
+// TEST CASES
+convertMe([['Tom', 'Cruise', 'Male', 1962], ['Johnny', 'Deep', 'Male']]);
+// 1. Tom Cruise:
+// { firstName: 'Tom',
+//   lastName: 'Cruise',
+//   gender: 'Male',
+//   age: 37 }
+// 2. Johnny Deep:
+// { firstName: 'Johnny',
+//   lastName: 'Deep',
+//   gender: 'Male',
+//   age: 'Invalid Birth Year' }
+convertMe([]); // ""
